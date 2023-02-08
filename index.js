06294421d0810406c4aed6af2125769df38d5a2c
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-08 17:04:37 +0530

    added clicked sound to all the elements

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ var btns = document.querySelectorAll(".drum");
 
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+
+    //playSound( "sounds/crash.mp3" );
+    
+    console.log(this);
+    console.log( this.innerHTML);
+
+    makeSound(this.innerHTML);
   });
+}
+
+function playSound( path ){
+  var audio = new Audio( path );
+  audio.play();
+}
+
+function makeSound( key ){
+
+  switch( key ){
+    case "w":
+      playSound( "sounds/crash.mp3" );
+      break;
+
+    case "a":
+      playSound( "sounds/kick-bass.mp3" );
+      break;
+    
+      case "s":
+      playSound( "sounds/snare.mp3" );
+      break;
+
+      case "d":
+      playSound( "sounds/tom-1.mp3" );
+      break;
+
+      case "j":
+      playSound( "sounds/tom-2.mp3" );
+      break;
+
+      case "k":
+      playSound( "sounds/tom-3.mp3" );
+      break;
+
+      case "l":
+      playSound( "sounds/tom-4.mp3" );
+      break;
+
+      default :
+      console.log(key);
+  }
+
 }
